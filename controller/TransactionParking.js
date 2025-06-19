ff16542109886c9bc8413c155c4c2bae2b4a4a2c
tomy21 <tomy21.agung@gmail.com>
2025-06-19 09:30:17 +0700

    updated

--- controller/TransactionParking.js
+++ controller/TransactionParking.js
@@ -184,6 +184,7 @@ export const updateTransactionStatus = async (req, res) => {
 
     const transaction = await Transaction.findOne({
       where: {
+        status: "In",
         [Op.or]: [
           { TransactionCode: TransactionCode },
           { RefNumber: TransactionCode },
