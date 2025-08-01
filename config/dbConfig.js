bc0c6b4f28bedd980391bf1954e3027ec582e5d5
tomy21 <tomy21.agung@gmail.com>
2025-08-01 18:21:30 +0700

    updated

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -13,24 +13,3 @@ export const dbOCC = new Sequelize(
     timezone: "+07:00",
   }
 );
-
-export const dbUnikas = new Sequelize(
-  process.env.DB_NAME_UNIKAS,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    logging: false,
-    timezone: "+07:00",
-  }
-);
-
-dbUnikas
-  .authenticate()
-  .then(() => {
-    console.log("Koneksi ke database Unikas berhasil.");
-  })
-  .catch((error) => {
-    console.error("Gagal terhubung ke database Unikas:", error);
-  });
