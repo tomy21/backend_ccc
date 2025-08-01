44f38941b1f24f27181a68668677176ce3aeba0d
tomy21 <tomy21.agung@gmail.com>
2025-08-01 18:26:26 +0700

    updated

--- controller/GateController.js
+++ controller/GateController.js
@@ -231,7 +231,8 @@ const processQueue = async () => {
       dataIssues: { dataGate: gate, dataIssues: issueResponse },
     });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    // res.status(500).json({ message: error.message });
+    console.log("error");
   } finally {
     processQueue(); // Proses permintaan berikutnya dalam antrean
   }
